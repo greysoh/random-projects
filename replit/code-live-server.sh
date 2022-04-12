bda74619f6f74c57b7faf670d37aee256c4fa380
Greyson <greysoh@pm.me>
2022-04-12 15:11:03 -0400

    chore: Uploads vscode server for replit.

---
+++ replit/code-live-server.sh
@@ -0,0 +1,44 @@
+if [ ! -f "/tmp/isNixConfigured" ]; then
+  echo "Reconfiguring nix..."
+
+  echo "{ pkgs }: {" > replit.nix
+  echo "deps = [" >> replit.nix
+  echo "        pkgs.bashInteractive" >> replit.nix
+  echo "        pkgs.wget" >> replit.nix
+  
+  if [ "pkgs.nix.txt" ]; then
+    cat pkgs.nix.txt >> replit.nix 
+    printf "\n" >> replit.nix
+  done
+
+  echo "    ];" >> replit.nix
+  echo "}" >> replit.nix
+
+  touch /tmp/isNixConfigured 
+  echo "Reconfigured. Please rerun this repl."
+  exit
+fi
+
+if [ ! -d "code_server" ]; then
+  echo "Downloading code server..."
+  curl -s https://api.github.com/repos/coder/code-server/releases/latest \
+    | grep "browser_download_url.*-linux-amd64.tar.gz" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
+  echo "Extracting code server..."
+  mkdir code_server
+  cp -r code-server-* code_server/
+  cd code_server
+  tar -xzvf code-server-*
+  rm -rf code-server-*.tar.gz ../code-server-*.tar.gz
+  echo "Installing code server..."
+  cd ..
+  mkdir -p .config/code-server
+  echo "bind-addr: 0.0.0.0:8000" > .config/code-server/config.yaml
+  echo "auth: none" >> .config/code-server/config.yaml
+  echo "cert: false" >> .config/code-server/config.yaml
+fi
+
+echo "Starting code server..."
+code_server/code-s*/bin/code-server
